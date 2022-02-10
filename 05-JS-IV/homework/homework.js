1923effa05cfcb4bc76bf731d64d99903306cacc
Lia <liadesbe@gmail.com>
2022-02-10 08:12:30 -0300

    prueba 3

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -155,7 +155,9 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto['calcularPrecioDescuento'] = function() {
+    return producto['precio'] * (1-producto['porcentajeDeDescuento']);
+  }
 }
 
 // No modificar nada debajo de esta línea
