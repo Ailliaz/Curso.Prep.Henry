219567b50f5faa853752a9b81da2080e82fca1bb
Lia <liadesbe@gmail.com>
2022-02-09 23:31:05 -0300

    prueba 5

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -123,6 +123,16 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  let multiply = 1;
+  if (arguments.length === 0) {
+    return 0;
+  }
+  else { 
+    for (let i = 0 ; i < arguments.length ; i++){
+      multiply = multiply * arguments[i];
+    }
+  }
+  return multiply;
 }
 
 
