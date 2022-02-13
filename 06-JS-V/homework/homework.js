9eae4dc84133842f98278b1d7a7e608b9cf2c8de
Lia <liadesbe@gmail.com>
2022-02-12 22:47:18 -0300

    prueba 5.1

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -81,7 +81,7 @@ function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
   Persona.prototype.datos = function(){
-    return Nombre + ", " + Edad + "años" 
+    return nombre + ", " + edad + "años" 
   }
 }
   
